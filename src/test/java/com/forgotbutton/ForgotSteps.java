d39fec0012d94f84067caf30107b2ae5bbf94ac3
jaigithub999 <jayaselvam999@gmail.com>
2023-08-27 02:32:14 +0530

    new linw of code added

---
+++ src/test/java/com/forgotbutton/ForgotSteps.java
@@ -0,0 +1,12 @@
+package com.forgotbutton;
+
+public class ForgotSteps {
+public ForgotSteps() {
+
+System.out.println("test 1 constructor");
+}
+public static void main(String[] args) {
+	ForgotSteps f = new ForgotSteps();
+	
+}
+}
